fa6fea1067bb0ca4d918c9ff12b3751f824e86ff
dimasadihartomo <dimasadi1308@gmail.com>
2021-03-19 10:42:41 +0700

    Add CardView

--- src/pages/RobotList.js
+++ src/pages/RobotList.js
@@ -2,16 +2,33 @@ import React, {useEffect} from 'react'
 import { connect } from 'react-redux'
 import { getUsers } from "../actions/userAction";
 
-const RobotList = (props) => {
+function CardView({data}) {
+
+    return (
+        <div className="col">
+            <div className="card h-100">
+                    <div className="card-body">
+                        <h5 className="d-inline-block">{data.name}</h5>
+                        <a href="#" className="btn btn-primary" style={{float: "right"}}>Get Info</a>
+                        <p className="card-text">This is a longer card with supporting text below as a natural lead-in
+                            to additional content. This content is a little bit longer.</p>
+                    </div>
+                <img src="..." className="card-img-top" alt="..."/>
+            </div>
+        </div>
+    )
+}
+
+function RobotList (props) {
 
     useEffect(() => {
         props.getUsers()
     })
 
     return (
-        <div>
+        <div className="row row-cols-1 row-cols-md-3 g-4" >
             {props.loading ? "Loading..." : props.error ? props.error.message :
-                props.data.map(u => <h3>{u.name}</h3>)}
+                props.data.map((u, i) => <CardView key={i} data={u}/>)}
         </div>
     )
 }
