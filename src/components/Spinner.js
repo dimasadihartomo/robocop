55327fc2d80a655bb9252106b44b93675471a79f
dimasadihartomo <dimasadi1308@gmail.com>
2021-04-19 10:35:52 +0700

    Modify .idea

--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Spinner() {
 
     return (
-        <div style={{marginLeft: "50%", marginTop: "20%", marginBottom: "18.2%"}}>
+        <div style={{marginLeft: "50%", marginTop: "13%", marginBottom: "18.2%"}}>
             <div className="spinner-grow" style={{width: "3rem", height: "3rem"}} role="status">
             </div>
         </div>
